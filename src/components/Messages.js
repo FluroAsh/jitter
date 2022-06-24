fe2c0dec07510922d471bd51e6328384f77be94e
Ashley Thompson <workashleythompson@gmail.com>
2022-06-24 16:24:57 +1000

    comment out env key

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -56,7 +56,6 @@ export const Messages = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]); // 3. Fire side effect if query string changes
 
-  console.log(messageList);
   return (
     <>
       {messageList.length > 0 && !error ? (
